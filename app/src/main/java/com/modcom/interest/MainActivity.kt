819b685c8d59224cd9837f881fb2621f5be0cbcb
modcomlearning <modcomlearning@gmail.com>
2021-03-30 11:03:36 +0300

    Third Commit

--- app/src/main/java/com/modcom/interest/MainActivity.kt
+++ app/src/main/java/com/modcom/interest/MainActivity.kt
@@ -10,19 +10,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //Access ids in xml
-        calculate.setOnClickListener({
+        calculate.setOnClickListener{
 
+            //get ids from xml, get their values, to string tehn to double or to int
             val p = principle.text.toString().toDouble()
             val r = rate.text.toString().toDouble()
             val t = time.text.toString().toDouble()
 
+            //do maths
             val interest = (p * r  *t /100)
 
             //now put the interest to answer text view
-            answer.text = "Interest $interest"
-        })
+            answer.text = "Interest $interest KES"
 
-
+        }//end
 
 
     }
